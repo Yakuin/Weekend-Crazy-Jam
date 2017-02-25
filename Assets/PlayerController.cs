fb7de22c2694bab86e5fd88011f4da4a7f0d4ee1
BOUALLAG Yassine <bouallag.yassine@laposte.net>
2017-02-25 16:21:21 +0100

    Basic ship movement and planet gravity

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float acceleration;
+	public float rotationSpeed = 30f;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		Rigidbody rb = GetComponent<Rigidbody>();
+		Vector3 up = transform.up * Input.GetAxis ("Vertical");
+
+		rb.AddForce(up * acceleration);
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+			transform.Rotate(-Vector3.back * rotationSpeed * Time.deltaTime);
+		else if (Input.GetKey(KeyCode.RightArrow))
+			transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/PlayerFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 playerpos = player.transform.position;
+		playerpos.z -= 100;
+		transform.position = playerpos;
+	}
+}
