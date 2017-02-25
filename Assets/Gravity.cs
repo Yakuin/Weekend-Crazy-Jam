08603779365b5e987ee9067a71130ebbfc3c2763
BOUALLAG Yassine <bouallag.yassine@laposte.net>
2017-02-25 14:05:23 +0100

    Basic stuff

---
+++ Assets/Gravity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+	public int gravity;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		var direction = -(other.attachedRigidbody.transform.position - transform.position);
+		var distance = Vector3.Distance(other.transform.position, transform.position);
+		if (other.attachedRigidbody)
+		{
+			other.attachedRigidbody.AddForce(direction * gravity / distance);
+		}
+
+	}
+		
+}
---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter (Collision other)
+	{
+		if (other.gameObject.tag == "Planet")
+			Debug.Log("Ouch");
+	}
+}
